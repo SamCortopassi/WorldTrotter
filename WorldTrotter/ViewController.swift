a570c9459bb4782a1903f9b26cb2fc1809d2d382
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-05-16 12:19:14 -0700

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Samantha Cortopassi on 5/16/19.
+//  Copyright © 2019 Samantha Cortopassi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
